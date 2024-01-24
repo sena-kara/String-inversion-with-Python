ff18792a54ea3cb531f14c5861aee47363e3baba
Senakara <106998274+sena-kara@users.noreply.github.com>
2024-01-24 21:52:31 +0300

    Add files via upload
    String inversion with Python

---
+++ String ters çevirme.py
@@ -0,0 +1,10 @@
+def FirstReverse(strParam):
+
+
+  # code goes here
+  return strParam
+list= ("Hello World and Coders")
+print(list[::-1])
+
+# keep this function call here 
+print(FirstReverse(input()))
